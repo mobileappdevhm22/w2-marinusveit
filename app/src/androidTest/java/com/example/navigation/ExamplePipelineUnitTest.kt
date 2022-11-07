e57f270a8b38b5f1fbdd20c7316a448c75190e10
Marinus Veit <marinus.veit@isys.de>
2022-11-07 14:39:42 +0100

    add instrumentation test and rename package

---
+++ app/src/androidTest/java/com/example/navigation/ExamplePipelineUnitTest.kt
@@ -0,0 +1,21 @@
+package com.example.w2_marinusveit.navigation
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(androidx.test.runner.AndroidJUnit4::class)
+class ExamplePipelineUnitTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = androidx.test.platform.app.InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.w2_marinusveit.navigation", appContext.packageName)
+    }
+}
